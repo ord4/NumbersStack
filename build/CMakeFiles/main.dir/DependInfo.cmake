e138597c4a4079397c009005ec035c56e9fc6351
Orion Davis <ord4@zips.uakron.edu>
2017-03-13 22:12:24 -0400

    Need to fix multAll and andAll

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/orion/Documents/code/cs2/NumbersStack/NumStack.cpp" "/home/orion/Documents/code/cs2/NumbersStack/build/CMakeFiles/main.dir/NumStack.cpp.o"
+  "/home/orion/Documents/code/cs2/NumbersStack/main_driver.cpp" "/home/orion/Documents/code/cs2/NumbersStack/build/CMakeFiles/main.dir/main_driver.cpp.o"
+  "/home/orion/Documents/code/cs2/NumbersStack/stack.cpp" "/home/orion/Documents/code/cs2/NumbersStack/build/CMakeFiles/main.dir/stack.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/stack.cpp.o"
+  "CMakeFiles/main.dir/NumStack.cpp.o"
+  "CMakeFiles/main.dir/main_driver.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
